780cf3e98a1e92c854490d457936f4588bd8bf46
hou <1455203932@qq.com>
2022-03-05 20:50:36 +0800

    SSM-CRUD

---
+++ src/test/java/com/hou/test/GitTest.java
@@ -0,0 +1,15 @@
+package com.hou.test;
+
+import org.junit.Test;
+
+/**
+ * @author shadow
+ * @Description
+ * @create 2022-03-05 20:36
+ */
+public class GitTest {
+    @Test
+    public void test() {
+        System.out.println("Github");
+    }
+}
